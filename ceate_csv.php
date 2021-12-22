6647f6cbf16a64b033a9bc491835607326843527
igoradmtg <85247883+igoradmtg@users.noreply.github.com>
2021-12-22 08:35:45 +0700

    Add files via upload

--- ceate_csv.php
+++ ceate_csv.php
@@ -20,3 +20,48 @@ for($a = 1; $a <= $max_row; $a ++) {
     echo "Row: $a" . PHP_EOL;
 }
 fclose($fh);
+
+$filename_csv = __DIR__ . '/test_column4.csv';
+$fh = fopen($filename_csv,'wb');
+if ($fh == false) {
+    exit("Error create file $filename_csv");
+}
+$ar_rows = ['Param1','Param2','Param3','Param4'];
+for($a = 1; $a <= $max_row; $a ++) {
+    $param1 = mt_rand($min_rnd,$max_rnd);
+    $param2 = mt_rand($min_rnd,$max_rnd);
+    $param3 = mt_rand($min_rnd,$max_rnd);
+    $param4 = $param1 + $param2 + $param3;
+    fputcsv($fh,[
+        $param1,
+        $param2,
+        $param3,
+        $param4
+    ]);
+    echo "Row: $a" . PHP_EOL;
+}
+fclose($fh);
+
+
+$filename_csv = __DIR__ . '/test_column5.csv';
+$fh = fopen($filename_csv,'wb');
+if ($fh == false) {
+    exit("Error create file $filename_csv");
+}
+$ar_rows = ['Param1','Param2','Param3','Param4','Param5'];
+for($a = 1; $a <= $max_row; $a ++) {
+    $param1 = mt_rand($min_rnd,$max_rnd); // Random number 1
+    $param2 = mt_rand($min_rnd,$max_rnd); // Random number 2
+    $param3 = mt_rand($min_rnd,$max_rnd); // Random number 3
+    $param4 = mt_rand($min_rnd,$max_rnd); // Random number 4
+    $param5 = $param1 + $param2 + $param3 * 2 + $param4; // Summ Rnd1 + Rnd2 + Rnd3 * 2 + Rnd4
+    fputcsv($fh,[
+        $param1,
+        $param2,
+        $param3,
+        $param4,
+        $param5
+    ]);
+    echo "Row: $a" . PHP_EOL;
+}
+fclose($fh);
